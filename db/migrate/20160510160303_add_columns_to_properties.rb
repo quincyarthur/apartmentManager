cd31aa06e3a29a8b15ac77b7904b97649f840987
Quincy Arthur <quincyarthur@gmail.com>
2016-06-15 13:58:22 -0400

    changes to migration files

--- db/migrate/20160510160303_add_columns_to_properties.rb
+++ db/migrate/20160510160303_add_columns_to_properties.rb
@@ -1,13 +1,13 @@
 class AddColumnToProperties < ActiveRecord::Migration
   def change
-    add_column :properties, :landlord_id, :int
-    add_column :properties, :category_id, :int
+    add_column :properties, :landlord_id, :integer
+    add_column :properties, :category_id, :integer
     add_column :properties, :island, :string
     add_column :properties, :street_name, :string
-    add_column :properties, :num_bedrooms, :int
-    add_column :properties, :num_bathrooms, :int
-    add_column :properties, :num_units, :int
-    add_column :properties, :monthly_amt, :int
+    add_column :properties, :num_bedrooms, :integer
+    add_column :properties, :num_bathrooms, :integer
+    add_column :properties, :num_units, :integer
+    add_column :properties, :monthly_amt, :integer
     add_column :properties, :description, :string
   end
 end
--- db/migrate/20160511150056_add_columns_to_tenants.rb
+++ db/migrate/20160511150056_add_columns_to_tenants.rb
@@ -2,7 +2,7 @@ class AddColumnsToTenants < ActiveRecord::Migration
   def change
     add_column :tenants, :first_name, :string
     add_column :tenants, :last_name, :string 
-    add_column :tenants, :phone_number, :int 
-    add_column :tenants, :balance, :int
+    add_column :tenants, :phone_number, :integer 
+    add_column :tenants, :balance, :integer
   end
 end
