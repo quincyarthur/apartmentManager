a2dec931fd740e4fb60d09b498b12adcf91ac8e2
Quincy Arthur <quincyarthur@gmail.com>
2016-06-16 10:41:04 -0400

    modified migrations

--- db/migrate/20160524034544_add_polymorphic_relationships.rb
+++ db/migrate/20160524034544_add_polymorphic_relationships.rb
@@ -2,5 +2,6 @@ class AddPolymorphicRelationships< ActiveRecord::Migration
   def change
     add_column :tenant_rent_details, :property_type, :string
     add_column :property_amenities, :property_type, :string
+    add_column :prospective_tenants, :property_type, :string
   end
 end
